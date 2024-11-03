f0e058b205400f5e5ba956e1f9059ff460e91b17
Mateus Ferreira <mateusaparecidoferreira@outlook.com>
2024-11-03 13:59:45 -0300

    Corrigido o header em telas mobile

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -8,6 +8,7 @@ import "./styles.css";
 const Header = () => {
   const [userInfo, setUserInfo] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para controlar o menu
 
   useEffect(() => {
     const userData = localStorage.getItem("userData");
@@ -15,7 +16,7 @@ const Header = () => {
       const userDataArray = JSON.parse(userData);
       const { id, nome, email, roles } = userDataArray[0];
       setUserInfo({ id, nome, email, roles });
-      setIsAdmin(roles === 2); //alterado
+      setIsAdmin(roles === 2);
     } else {
       setUserInfo(null);
       setIsAdmin(false);
@@ -23,9 +24,14 @@ const Header = () => {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem("userData"); // Remove os dados do usuário
+    localStorage.removeItem("userData");
     setUserInfo(null);
     setIsAdmin(false);
+  };
+
+  // Função para alternar o menu
+  const toggleMenu = () => {
+    setIsMenuOpen(prevState => !prevState); // Alterna o estado do menu
   };
 
   return (
@@ -46,7 +52,12 @@ const Header = () => {
               )}
             </figure>
 
-            <div className="nav__button">
+            <button className="menu-button" onClick={toggleMenu}>
+              ☰ {/* Ícone de menu */}
+            </button>
+
+            {/* Renderização condicional para desktop e mobile */}
+            <div className={`nav__button ${isMenuOpen ? 'menu-active' : ''}`}>
               {userInfo && (
                 <Link to="/cadastro-viagem">Cadastro de Viagens</Link>
               )}
@@ -57,30 +68,28 @@ const Header = () => {
               {isAdmin && (
                 <Link to="/consulta-administrador">Consultar Usuários</Link>
               )}
-
               {userInfo ? (
-                <div className="user-info">
+                <><div className="user-info">
                   <figure>
                     <Link to="/menu">
                       <img
                         className="image-name"
                         src={usuerOn}
                         alt="Online"
-                        title="Online"
-                      />
+                        title="Online" />
                     </Link>
                   </figure>
                   <span className="user-name">{userInfo.nome}</span>
-                  <Link to="/" onClick={handleLogout}>
-                    Sair
-                  </Link>
-                </div>
+                </div><div className="logout-container">
+                    <Link to="/" onClick={handleLogout} className="logout-link">
+                      Sair
+                    </Link>
+                  </div></>
               ) : (
                 <Link to="/login">Entrar</Link>
               )}
             </div>
           </nav>
-          {/* Adicionando o botão de acessibilidade */}
           <AccessibilityButton />
         </div>
       </header>
